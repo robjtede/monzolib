362f98a7dd76f10d879263ea99cfca62a6ebabfb
Rob Ede <robjtede@icloud.com>
2017-08-27 23:07:25 +0100

    migrate lib from monux

---
+++ api.ts
@@ -0,0 +1,44 @@
+import { Primitive } from './lib/json-types'
+
+export type HttpMethods = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'OPTIONS'
+
+export interface QueryString {
+  [key: string]: Primitive | undefined
+}
+
+export interface MonzoRequest {
+  path: string
+  qs?: QueryString
+  method?: HttpMethods
+  json?: boolean
+}
+
+export class MonzoApi {
+  private proto: string = 'https://'
+  private apiRoot: string = 'api.monzo.com'
+
+  constructor(private accessResponse: string) {}
+
+  public request(
+    {
+      path = '/ping/whoami',
+      qs = {},
+      method = 'GET',
+      json = true
+    }: MonzoRequest = { path: '/ping/whoami' }
+  ) {
+    const headers = {
+      Authorization: `Bearer ${this.accessResponse}`
+    }
+
+    const opts = {
+      method,
+      uri: `${this.proto}${this.apiRoot}${path}`,
+      [method === 'GET' ? 'qs' : 'form']: qs,
+      headers,
+      json
+    }
+
+    return rp(opts)
+  }
+}
---
+++ index.ts
@@ -0,0 +1,9 @@
+export * from './Account'
+export * from './Amount'
+export * from './Attachment'
+export * from './Merchant'
+export * from './Transaction'
+
+// export * from './api'
+// export * from './auth'
+export * from './helpers'
